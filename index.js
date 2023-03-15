e4712795748f8f1ae6c2cc316f49014c01536a86
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-03-15 18:01:46 -0300

    terminado, agregado boton para tab

--- index.js
+++ index.js
@@ -1,38 +1,55 @@
+let myLeads = [];
 const SAVE_INPUT = document.getElementById("input-btn");
 const IMPUT_FIELD = document.getElementById("input-el");
 const UL_FIELD = document.getElementById("ul-el");
-let myLeads = [];
+const DELETE_BTN = document.getElementById("delete-btn");
+const TAB_BTN = document.getElementById("tab-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
-
-if(leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage;
-    renderLeads();
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
 }
 
-SAVE_INPUT.addEventListener("click", function () {
-  myLeads.push(IMPUT_FIELD.value);
-  console.log(myLeads);
-  IMPUT_FIELD.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-  renderLeads();
+const tabs = [{ url: "https://www.linkedin.com/in/per-harald-borgen/" }];
 
-  console.log(localStorage.getItem("myLeads"))
+TAB_BTN.addEventListener("click", function () {
+  //   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  //   })
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
-    <li>
-        <a target = _blank href=https://${myLeads[i]}>
-            ${myLeads[i]}
-        </a>
-    </li>
-    `;
+      <li>
+          <a target = _blank href=${leads[i]}>
+              ${leads[i]}
+          </a>
+      </li>
+      `;
     console.log(listItems);
   }
 
   UL_FIELD.innerHTML = listItems;
   IMPUT_FIELD.textContent = "se agrego";
 }
+
+DELETE_BTN.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+SAVE_INPUT.addEventListener("click", function () {
+  myLeads.push(IMPUT_FIELD.value);
+  IMPUT_FIELD.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
