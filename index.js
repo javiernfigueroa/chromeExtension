f4606c97487a4201bb3e11ddfdd6f051268e3a3b
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-03-15 17:12:18 -0300

    delete btn

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ const IMPUT_FIELD = document.getElementById("input-el");
 const UL_FIELD = document.getElementById("ul-el");
 let myLeads = [];
 
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    renderLeads();
+}
+
 SAVE_INPUT.addEventListener("click", function () {
   myLeads.push(IMPUT_FIELD.value);
   console.log(myLeads);
   IMPUT_FIELD.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads();
+
+  console.log(localStorage.getItem("myLeads"))
 });
 
 function renderLeads() {
